fcde4faa407de7b2deb2d5538208588f6307aa62
Yujin Jeong <express.yujin@gmail.com>
2021-11-25 02:37:04 +0900

    add img to local header page

--- pages/local.vue
+++ pages/local.vue
@@ -1,7 +1,28 @@
 <template>
-  <div>local</div>
+  <div class="local">
+    <Navi />
+    <div class="section">
+      <img src="/found-local-header.png" />
+      <img src="/found-local-balls.png" />
+      <h1>
+        전국 8도 로컬 크리에이터와 <br />
+        파트너가 되고 싶습니다
+      </h1>
+      <p>
+        전국 8 도 로컬 크리에이터의 드래곤 볼을 하나로 모으고 싶습니다.
+        혼자보다는 여럿이 함께 였을 때의 힘을 믿거든요. 파운드마켓 다람쥐 사원과
+        함께 세상에 우리가 있음을 이야기할 분들을 찾고 있습니다.
+        <br /><br />
+        로컬 크리에이터의 영역은 정해져 있지 않습니다. 로컬을 기반으로 한 제품일
+        수도 있고, 공간일 수도 있으며,서비스나 프로그램이어도 좋습니다. 제안해
+        주세요.
+        <br /><br />
+        다람쥐 사원이 함께 고민하겠습니다.
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
